12537f7830d7a231407e8763fd1ed23f1506b2ec
nhkim085 <nhkim@tma.com.vn>
2013-10-02 15:08:23 +0700

    Renew

--- parsingandimport/demoxl.js
+++ parsingandimport/demoxl.js
@@ -8,6 +8,21 @@ var async = require('async'),
 var db=require('riak-js').getClient({host: "192.168.103.222", port: "8098"});
 var sys=require('sys');
 var index=0;
+var headerpp=[];
+var datapp={Employee: []};
+function handleRow(rows,callback){
+	rows.forEach(function(cell){
+		
+		if(cell.row===0){			
+			headerpp.push(cell.value+'');
+		}
+	});
+	callback(rows);
+};
+
+function replace(key, value) {
+    return value.toString();
+};
 //async.eachSeries(sampleFiles, function (file, next) {
 //		xlrd.parse(file,{ sheet: 0, maxRows: 10 }, function (err, workbook) {
 //
@@ -31,8 +46,8 @@ xlrd.stream(sampleFiles).on('open', function (workbook) {
 
 //	console.log(crrSh.name);
 	data.rows.forEach(function(row){
-		console.log(row);
-		row.forEach(function (cell) {
+//		console.log(row);
+//		row.forEach(function (cell) {
 //				console.log(cell.address + ': ' + cell.value);
 //			if(cell.value!==null){
 //				index++;
@@ -41,10 +56,41 @@ xlrd.stream(sampleFiles).on('open', function (workbook) {
 //					console.log('Added '+index+' : '+cell.value);
 //				});
 //			}
+//			
+//		});
+					
 			
+		handleRow(row,function(rows){
+			var datarow=[];	
+			rows.forEach(function(cell){
+				if(cell.row!==0){					
+						var i=rows.indexOf(cell);
+						
+						if(i<headerpp.length){
+						var head=headerpp[i]+"";
+										
+						var bb={};
+						bb[head]=cell.value;						
+						datarow.push(bb);
+					}
+				}
+			});
+			if(datarow.length!==0)				
+			console.log(datarow);
+			
+				
+			
+			
+//			datapp.push(datarow);
+//			datapp["Employee"]=datarow;
+			
+//			if(datarow.length!==0)
+//			datapp["Employee"].push(datarow);
 		});
-
+		
+		
 	});
+//	console.log(datapp["Employee"]);
 
 
 }).on('error', function (err) {
@@ -53,4 +99,5 @@ xlrd.stream(sampleFiles).on('open', function (workbook) {
 //	 TODO: finishing logic here
 	console.log('END----------');
 });
+
 
